ebfb7e62f42dc229e366926d449447c0536f713e
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-09-18 10:09:56 -0400

    add cloudinary for upload 🚀

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -19,5 +19,20 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
   // ...
 });
